5a6f7436b732b972115299137d848825d9e71a3f
mrevelo <59065739+mbo-mrevelo@users.noreply.github.com>
2020-12-17 15:35:10 -0800

    moved files into the source folder

--- models/staging/mbo_client_prep/stg_payment_types.sql
+++
@@ -1,6 +0,0 @@
-select
-    studioid,
-    item#,
-    casheq
-
-from {{ source('mbo_client_prep', 'payment_types') }}
--- models/staging/mbo_client_prep/stg_tblpayments.sql
+++
@@ -1,8 +0,0 @@
-select
-    studioid,
-    saleid,
-    paymentamount,
-    paymenttax,
-    paymentmethod
-
-from {{ source('mbo_client_prep', 'tblpayments') }}
